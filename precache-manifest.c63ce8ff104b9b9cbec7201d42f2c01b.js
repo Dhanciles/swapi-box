7fabed949123e3c5c8cc2c28cae97fac26a8c1fd
Derek Hanciles <drhanciles@icloud.com>
2019-01-06 19:51:02 -0700

    Updates

---
+++ precache-manifest.c63ce8ff104b9b9cbec7201d42f2c01b.js
@@ -0,0 +1,174 @@
+self.__precacheManifest = [
+  {
+    "revision": "db4c9824257caea84da528727f2932e5",
+    "url": "/SWAPI-box/static/media/bespin.db4c9824.jpg"
+  },
+  {
+    "revision": "fad2e381c3c0b80366f1",
+    "url": "/SWAPI-box/static/css/main.8cbecb64.chunk.css"
+  },
+  {
+    "revision": "1032b257eeb0db8120ec",
+    "url": "/SWAPI-box/static/js/1.1032b257.chunk.js"
+  },
+  {
+    "revision": "05ffcd0487c4c1e751b5",
+    "url": "/SWAPI-box/static/js/runtime~main.05ffcd04.js"
+  },
+  {
+    "revision": "ea5869c94d37c9efd1bedea5147d7722",
+    "url": "/SWAPI-box/static/media/twinkling.ea5869c9.png"
+  },
+  {
+    "revision": "ad1735df3de25d97b311da8881e48d5c",
+    "url": "/SWAPI-box/static/media/stars-background.ad1735df.jpg"
+  },
+  {
+    "revision": "7084cf76249e3496aeebc9b0ff67d30e",
+    "url": "/SWAPI-box/static/media/beru-lars.7084cf76.jpg"
+  },
+  {
+    "revision": "540f585060447b2afa1ac8377b1a75dd",
+    "url": "/SWAPI-box/static/media/luke.540f5850.jpg"
+  },
+  {
+    "revision": "c393bf90214ebd20f89bd87c24150267",
+    "url": "/SWAPI-box/static/media/c3po.c393bf90.jpeg"
+  },
+  {
+    "revision": "0cdd36621d44f4a7f5a9312f5c3d91db",
+    "url": "/SWAPI-box/static/media/r2d2.0cdd3662.jpg"
+  },
+  {
+    "revision": "f69803cd2bd9b6a093e01aebc4adadaa",
+    "url": "/SWAPI-box/static/media/darth-vader.f69803cd.jpg"
+  },
+  {
+    "revision": "6b8c1f7dbc8d1b4f7dfae25b668eddd2",
+    "url": "/SWAPI-box/static/media/leia-organa.6b8c1f7d.jpeg"
+  },
+  {
+    "revision": "0590958c7d93edffa88c45ec89e6647c",
+    "url": "/SWAPI-box/static/media/owen-lars.0590958c.jpg"
+  },
+  {
+    "revision": "63133e71714231f5dddbab3e3f34a286",
+    "url": "/SWAPI-box/static/media/r5-d4.63133e71.jpg"
+  },
+  {
+    "revision": "6ed06b55415bb0072020a544927ef005",
+    "url": "/SWAPI-box/static/media/biggs-darklighter.6ed06b55.jpg"
+  },
+  {
+    "revision": "75a9b966e93e68742c1841d7da19bbc7",
+    "url": "/SWAPI-box/static/media/obi-wan-kenobi.75a9b966.jpg"
+  },
+  {
+    "revision": "dd24fa0f23b412c1fd2bf16c64a1ca90",
+    "url": "/SWAPI-box/static/media/a-wing.dd24fa0f.jpg"
+  },
+  {
+    "revision": "0f6598fcf6093604cac10d9c0ac68f3f",
+    "url": "/SWAPI-box/static/media/alderaan.0f6598fc.jpg"
+  },
+  {
+    "revision": "3c8be0f790482bf2d200f60cf68f0112",
+    "url": "/SWAPI-box/static/media/at-at.3c8be0f7.jpg"
+  },
+  {
+    "revision": "5b9635a46789aff27049df50ffccb5ab",
+    "url": "/SWAPI-box/static/media/at-st.5b9635a4.jpg"
+  },
+  {
+    "revision": "a733bd9f63903f2140fda9c63164113d",
+    "url": "/SWAPI-box/static/media/b-wing.a733bd9f.jpg"
+  },
+  {
+    "revision": "fad2e381c3c0b80366f1",
+    "url": "/SWAPI-box/static/js/main.fad2e381.chunk.js"
+  },
+  {
+    "revision": "26e572866ff67aa8ed109a24b215e2b0",
+    "url": "/SWAPI-box/static/media/coruscant.26e57286.jpg"
+  },
+  {
+    "revision": "2e722a433ba97b862d8fe57f05bdb1a2",
+    "url": "/SWAPI-box/static/media/dagobah.2e722a43.jpg"
+  },
+  {
+    "revision": "82348d48a6d24282e0108a51bac2716b",
+    "url": "/SWAPI-box/static/media/endor.82348d48.jpg"
+  },
+  {
+    "revision": "f996aac44b3f5df741d338de28bc8e3e",
+    "url": "/SWAPI-box/static/media/geonosis.f996aac4.jpeg"
+  },
+  {
+    "revision": "57a9e1332e1255e5f77a6b0649a94ca0",
+    "url": "/SWAPI-box/static/media/hoth.57a9e133.jpg"
+  },
+  {
+    "revision": "d5aa43b0806ce7ee861cfaa0ce31715b",
+    "url": "/SWAPI-box/static/media/kamino.d5aa43b0.jpg"
+  },
+  {
+    "revision": "80ba447bb81a32c3e96b3d9ad4b7e0ea",
+    "url": "/SWAPI-box/static/media/naboo.80ba447b.jpg"
+  },
+  {
+    "revision": "7da9c5941b2c77bd4bf8e219b5eba908",
+    "url": "/SWAPI-box/static/media/sail-barge.7da9c594.jpeg"
+  },
+  {
+    "revision": "d3c6d88e0585c124d6517d18a620968c",
+    "url": "/SWAPI-box/static/media/sand-crawler.d3c6d88e.jpeg"
+  },
+  {
+    "revision": "04aeaf59bfa2ab5c60417847dfd0b864",
+    "url": "/SWAPI-box/static/media/snowspeeder.04aeaf59.png"
+  },
+  {
+    "revision": "72bf95bce4d38a98ce2ccb5b0c1fbd4d",
+    "url": "/SWAPI-box/static/media/storm-iv.72bf95bc.jpg"
+  },
+  {
+    "revision": "7d3c2dafca8cfda5e5e32955a71fb6be",
+    "url": "/SWAPI-box/static/media/t-16-skyhopper.7d3c2daf.jpg"
+  },
+  {
+    "revision": "b2cb84e95f61f5db80194462018597c3",
+    "url": "/SWAPI-box/static/media/tatooine.b2cb84e9.jpg"
+  },
+  {
+    "revision": "0741d299909d01e7948303ac85e526f1",
+    "url": "/SWAPI-box/static/media/tie-bomber.0741d299.jpg"
+  },
+  {
+    "revision": "8808cdff71f05022541fc76023d9ec66",
+    "url": "/SWAPI-box/static/media/tie-fighter.8808cdff.jpeg"
+  },
+  {
+    "revision": "e36e492198683326bf03ab7f7616d0c5",
+    "url": "/SWAPI-box/static/media/x-34-landspeeder.e36e4921.jpg"
+  },
+  {
+    "revision": "59cc2f0a4bf2ef982b26855c3901d869",
+    "url": "/SWAPI-box/static/media/x-wing.59cc2f0a.jpg"
+  },
+  {
+    "revision": "21a7a7009f22ae590e08575fef92ccb6",
+    "url": "/SWAPI-box/static/media/yavin-4.21a7a700.jpg"
+  },
+  {
+    "revision": "cc3019aa6439ef893839b8a0d6d0391d",
+    "url": "/SWAPI-box/static/media/Starjedi.cc3019aa.ttf"
+  },
+  {
+    "revision": "11131ba3a339089d239c2f5c0594cf8c",
+    "url": "/SWAPI-box/static/media/hallway.11131ba3.jpeg"
+  },
+  {
+    "revision": "f14b461064bfeb4598a8c14687e91e58",
+    "url": "/SWAPI-box/index.html"
+  }
+];
